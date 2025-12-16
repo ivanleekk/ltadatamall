989d20de45fb90fee7f205faec67e5f7b15d9044
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2025-12-16 22:51:51 +0800

    feat: get all bus routes works

--- bus.go
+++ bus.go
@@ -25,10 +25,30 @@ type AllBusServiceResponse struct {
 	BusServices []BusService `json:"value"`
 }
 
+type BusRoute struct {
+	ServiceNo    string  `json:"ServiceNo"`
+	Operator     string  `json:"Operator"`
+	Direction    int     `json:"Direction"`
+	StopSequence int     `json:"StopSequence"`
+	BusStopCode  string  `json:"BusStopCode"`
+	Distance     float32 `json:"Distance"`
+	WD_FirstBus  string  `json:"WD_FirstBus"`
+	WD_LastBus   string  `json:"WD_LastBus"`
+	SAT_FirstBus string  `json:"SAT_FirstBus"`
+	SAT_LastBus  string  `json:"SAT_LastBus"`
+	SUN_FirstBus string  `json:"SUN_FirstBus"`
+	SUN_LastBus  string  `json:"SUN_LastBus"`
+}
+
+type AllBusRouteResponse struct {
+	Metadata  string     `json:"odata.metadata"`
+	BusRoutes []BusRoute `json:"value"`
+}
+
 func GetAllBusService(client *APIClient) (AllBusServiceResponse, error) {
 	req, err := http.NewRequest("GET", client.baseURL+"BusServices", nil)
 	if err != nil {
-		return AllBusServiceResponse{}, errors.New("Error creating the request")
+		return AllBusServiceResponse{}, errors.New("error creating the request")
 	}
 	req.Header.Add("AccountKey", client.accountKey)
 
@@ -42,6 +62,28 @@ func GetAllBusService(client *APIClient) (AllBusServiceResponse, error) {
 	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
 		return AllBusServiceResponse{}, err
+	}
+
+	return result, nil
+}
+
+func GetAllBusRoute(client *APIClient) (AllBusRouteResponse, error) {
+	req, err := http.NewRequest("GET", client.baseURL+"BusRoutes", nil)
+	if err != nil {
+		return AllBusRouteResponse{}, errors.New("error creating the request")
+	}
+	req.Header.Add("AccountKey", client.accountKey)
+
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return AllBusRouteResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var result AllBusRouteResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return AllBusRouteResponse{}, err
 	}
 
 	return result, nil
--- bus_test.go
+++ bus_test.go
@@ -34,3 +34,23 @@ func TestGetAllBusService(t *testing.T) {
 		t.Errorf("Expected non-empty BusServices in response")
 	}
 }
+
+func TestGetAllBusRoutes(t *testing.T) {
+	baseURL := os.Getenv("LTA_DATAMALL_BASE_URL")
+	accountKey := os.Getenv("LTA_DATAMALL_ACCOUNT_KEY")
+	if baseURL == "" || accountKey == "" {
+		t.Fatalf("Environment variables LTA_DATAMALL_BASE_URL and LTA_DATAMALL_ACCOUNT_KEY must be set")
+	}
+
+	client := NewClient(baseURL, accountKey)
+
+	response, err := GetAllBusRoute(client)
+
+	if err != nil {
+		t.Fatalf("Error calling GetAllBusRoute: %v", err)
+	}
+
+	if len(response.BusRoutes) == 0 {
+		t.Errorf("Expected non-empty BusRoutes in response")
+	}
+}
