1e2368287f286d3e1d4ba7c3dd9abce3ee6de37f
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2025-12-24 23:46:12 +0800

    feat: Forecast Station Crowd Densities APIS

--- station_crowd_density.go
+++ station_crowd_density.go
@@ -27,6 +27,28 @@ type AllRealTimeStationCrowdDensityResponse struct {
 	Lines []RealTimeStationCrowdDensityResponse
 }
 
+type CrowdLevelInterval struct {
+	Start      time.Time `json:"Start"`
+	CrowdLevel string    `json:"CrowdLevel"`
+}
+type ForecastStationCrowdDensity struct {
+	Station  string               `json:"Station"`
+	Interval []CrowdLevelInterval `json:"Interval"`
+}
+
+type DatedForecastStationCrowdDensity struct {
+	Date     time.Time                     `json:"Date"`
+	Stations []ForecastStationCrowdDensity `json:"Stations"`
+}
+type ForecastStationCrowdDensityResponse struct {
+	Metadata                    string `json:"odata.metadata"`
+	TrainLine                   string
+	ForecastStationCrowdDensity []DatedForecastStationCrowdDensity `json:"value"`
+}
+type AllForecastStationCrowdDensityResponse struct {
+	Lines []ForecastStationCrowdDensityResponse
+}
+
 func GetStationCrowdDensityRealTimeByLine(apiClient *APIClient, line string) (RealTimeStationCrowdDensityResponse, error) {
 	var result RealTimeStationCrowdDensityResponse
 	endpoint := "PCDRealTime?TrainLine=" + line
@@ -49,6 +71,35 @@ func GetAllStationCrowdDensityRealTime(apiClient *APIClient) (AllRealTimeStation
 		res, err := GetStationCrowdDensityRealTimeByLine(apiClient, AllTrainLines[line])
 		if err != nil {
 			return AllRealTimeStationCrowdDensityResponse{}, err
+		}
+
+		result.Lines = append(result.Lines, res)
+	}
+	return result, nil
+}
+
+func GetStationCrowdDensityForecastByLine(apiClient *APIClient, line string) (ForecastStationCrowdDensityResponse, error) {
+	var result ForecastStationCrowdDensityResponse
+	endpoint := "PCDForecast?TrainLine=" + line
+
+	if err := apiClient.getJSON(endpoint, &result); err != nil {
+		return ForecastStationCrowdDensityResponse{}, err
+	}
+
+	if len(result.ForecastStationCrowdDensity) == 0 {
+		return ForecastStationCrowdDensityResponse{}, errors.New("no station crowd densities available")
+	}
+	result.TrainLine = line
+	return result, nil
+
+}
+
+func GetAllStationCrowdDensityForecast(apiClient *APIClient) (AllForecastStationCrowdDensityResponse, error) {
+	var result AllForecastStationCrowdDensityResponse
+	for line := range AllTrainLines {
+		res, err := GetStationCrowdDensityForecastByLine(apiClient, AllTrainLines[line])
+		if err != nil {
+			return AllForecastStationCrowdDensityResponse{}, err
 		}
 
 		result.Lines = append(result.Lines, res)
--- station_crowd_density_test.go
+++ station_crowd_density_test.go
@@ -26,3 +26,25 @@ func TestGetStationCrowdDensityRealTimeByLine(t *testing.T) {
 		t.Errorf("Expected at least 27 RealTime Station Crowd Density in response")
 	}
 }
+
+func TestGetAllStationCrowdDensityForecast(t *testing.T) {
+	response, err := GetAllStationCrowdDensityForecast(testClient)
+	if err != nil {
+		t.Fatalf("Error calling GetAllStationCrowdDensityForecast: %v", err)
+	}
+	log.Default().Println("GetAllStationCrowdDensityForecast", response)
+	if len(response.Lines) != 11 {
+		t.Errorf("Expected Exactly 11 Lines of Forecast Station Crowd Density in response")
+	}
+}
+
+func TestGetStationCrowdDensityForecastByLine(t *testing.T) {
+	response, err := GetStationCrowdDensityForecastByLine(testClient, "TEL")
+	if err != nil {
+		t.Fatalf("Error calling GetStationCrowdDensityForecastByLine: %v", err)
+	}
+
+	if len(response.ForecastStationCrowdDensity[0].Stations) < 27 {
+		t.Errorf("Expected at least 27 Forecast Station Crowd Density in response")
+	}
+}
